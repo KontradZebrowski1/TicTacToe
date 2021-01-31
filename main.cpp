b10d85ce890e479421485c61a5bc12ba354672d0
KontradZebrowsk <konzebr@gmail.com>
2021-01-31 00:35:00 +0000

    minor things

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-char kwadratBazowy[9] = {'0','1','2','3','4','5','6','7','8'};
+char kwadratBazowy[9] = {'1','2','3','4','5','6','7','8','9'};
 //char
 
 int czyWygrana()
 {
 return 0;
 }
-int main()
+void plansza()
 {
-  std::cout << " 1 | 2 | 3 \n---|---|---\n 4 | 5 | 6 \n---|---|---\n 7 | 8 | 9 \n";
+  std::cout << " "<<kwadratBazowy[0]<<" | 2 | 3 \n---|---|---\n 4 | 5 | 6 \n---|---|---\n 7 | 8 | 9 \n";
 }
