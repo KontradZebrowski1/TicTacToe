480915a380ddfbe8fef8eafd8fc5bc77b6c870f6
KontradZebrowsk <konzebr@gmail.com>
2021-01-31 00:32:40 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+char kwadratBazowy[9] = {'0','1','2','3','4','5','6','7','8'};
+//char
+
+int czyWygrana()
+{
+return 0;
+}
+int main()
+{
+  std::cout << " 1 | 2 | 3 \n---|---|---\n 4 | 5 | 6 \n---|---|---\n 7 | 8 | 9 \n";
+}
